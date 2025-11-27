ad0911610feb714cab1768015b0f5b70ef60279a
dmose <100089505+janusdotai@users.noreply.github.com>
2025-11-27 13:57:54 -0500

    bt10_rc4

--- tests/test_llm_cloud.py
+++ tests/test_llm_cloud.py
@@ -730,8 +730,10 @@ def test_latest_openrouter_model():
     #model = "moonshotai/kimi-k2:free"
     #model = "openrouter/polaris-alpha"
     #model = "openrouter/sherlock-dash-alpha"
-    model = "openai/gpt-5.1-chat"
-    
+    #model = "openai/gpt-5.1-chat"
+    #model = "x-ai/grok-4.1-fast"
+    model = "openrouter/bert-nebulon-alpha"
+        
     provider = LLM.OPEN_ROUTER
     
     print(f"\033[32mTesting {provider} with model: {model} \033[0m")
@@ -1074,6 +1076,58 @@ def test_perplexity_inference():
         assert count == 1
     assert user_prompt not in skus
 
+
+
+@pytest.mark.skip(reason="skipped")
+def test_gemini_inference():
+    raw_products = product_woo()      
+    products = ProductFactory.dedupe(raw_products)    
+    rp = safe_random.choice(products)
+    user_prompt = rp.sku    
+    num_recs = safe_random.choice([3, 4, 5])
+    debug_prompts = False
+
+    match = [products for products in products if products.sku == user_prompt][0]
+    print(match)
+    print(f"\033[32mSelected product: {match.sku} - {match.name} \033[0m")
+
+    context = json.dumps([asdict(products) for products in products])
+    factory = PromptFactory(sku=user_prompt, 
+                            context=context, 
+                            num_recs=num_recs,
+                            debug=debug_prompts)
+    
+    prompt = factory.generate_prompt()    
+    print(f"PROMPT SIZE: {len(prompt)}") 
+    wc = PromptFactory.get_word_count(prompt)
+    print(f"word count: {wc}")
+    tc = PromptFactory.get_token_count(prompt)
+    print(f"token count: {tc}")       
+    
+    
+    model = "gemini-3-pro-preview"
+    provider = LLM.GEMINI
+    
+    print(f"\033[32mTesting {provider} with model: {model} \033[0m")
+    st = time.time()
+    llm_response = LLMFactory.query_llm(server=provider,
+                                 model=model,
+                                 system_prompt="You are a helpful assistant", 
+                                 temp=0.0, 
+                                 user_prompt=prompt)
+    et = time.time()
+    diff = et - st  
+    print(f"LLM response time: {diff:.2f} seconds")
+    parsed_recs = PromptFactory.tryparse_llm(llm_response)
+    print(f"parsed {len(parsed_recs)} records")
+    print(parsed_recs)
+    assert len(parsed_recs) == num_recs    
+    skus = [item['sku'] for item in parsed_recs]
+    counter = Counter(skus)
+    for sku, count in counter.items():
+        print(f"{sku}: {count}")
+        assert count == 1
+    assert user_prompt not in skus
 
 
 
